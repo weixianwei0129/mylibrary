c075442a925693f39a48b31780bdd95e6ef2da95
wxw <weixianwei0129@gmail.com>
2024-07-28 16:26:14 +0800

    update

--- wxw/common.py
+++ wxw/common.py
@@ -19,7 +19,6 @@ from collections import defaultdict
 import warnings
 import functools
 from PIL import Image
-from utils import deprecated
 from PIL import ImageFont, ImageDraw
 import matplotlib.font_manager as fm
 
@@ -59,6 +58,20 @@ class ONNXRunner:
 
 
 # =============基础方法===============
+
+def deprecated(func):
+    """这是一个装饰器，用于标记函数为已弃用。当使用该函数时，会发出警告。"""
+
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # 关闭过滤器
+        warnings.warn(f"调用已弃用的函数 {func.__name__}.", category=DeprecationWarning, stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # 恢复过滤器
+        return func(*args, **kwargs)
+
+    return new_func
+
+
 def md5sum(file_path):
     with open(file_path, "rb") as file:
         md5_hash = hashlib.md5()
